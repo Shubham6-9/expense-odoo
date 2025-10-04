d10f18f909d2da3b34b4ce11e3a05dd8bd32980c
R-Priyanshuofficial <rpriyanshugit@gmail.com>
2025-10-04 14:08:20 +0530

    bug fix 2

--- models/companyAdmin.model.js
+++ models/companyAdmin.model.js
@@ -32,7 +32,8 @@ const companyAdminSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    required : true,
+    required: true,
+    default: "admin",
   },
 }, { timestamps: true });
 
